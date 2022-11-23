774ebc2d4ed663c70a231902a3fa4c7955731e23
SofiaCadisco99 <113481279+SofiaCadisco99@users.noreply.github.com>
2022-11-23 11:10:42 +0100

    summary

---
+++ server.js
@@ -0,0 +1,14 @@
+console.log("script loaded");
+
+let express = require("express");
+
+//we are activating express
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+app.use(express.static("public"));
+
+console.log("running server on http://localhost:" + port);
